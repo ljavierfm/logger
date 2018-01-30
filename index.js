cda227e68e019603220b2f3046935e4c382639bb
Javier Fernandez <javier.fernandez@mobentis.com>
2018-01-30 10:38:58 +0100

    Se agregan directorio public, uploads y se configuran estos para express. Se crea índice de la app

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const _=require('lodash');
 const moment=require('moment');
 const fs=require('fs');
+const express=require('express');
+
+const app=express();
+app.use(express.static(__dirname+'/public/'));
+app.use(express.static(__dirname+'/uploads/'));
+
+
+
+
+app.listen(3000,()=>{
+    console.log('Express arrancado');
+});
